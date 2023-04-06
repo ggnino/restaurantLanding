e8021695cbdb5dc307ff3c71aca530780c06b171
nino Gonzalez <ggnino18@gmail.com>
2023-04-06 15:30:32 -0400

    added comments to script

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const headingElement = document.getElementById('home');
-const menuElement = document.getElementById('menu');
-const headingGroup = document.getElementById('hgroup');
-const aboutElement = document.getElementById('about-us');
-const menuHighlight = document.getElementById('menu-highlight');
-const burger = document.getElementById('burger');
-const pollo = document.getElementById('pollo');
-const bistec = document.getElementById('bistec');
-const text = menuHighlight.textContent;
+// Variables
+const menuHighlight = document.getElementById('menu-highlight'); // menu highlight element
+const burger = document.getElementById('burger'); // burger menu item element
+const pollo = document.getElementById('pollo'); // chicken menu item element
+const bistec = document.getElementById('bistec'); // steak menu item element
+const text = menuHighlight.textContent; // menu highlight elements text content
 
-console.log(window.innerWidth, window.innerHeight);
+// Hover effect for menu items
 const menuItemHover = (e) => {
-	const element = e.target.id;
-	const event = e.type;
-
+	const element = e.target.id; // menu item
+	const event = e.type; // event type
+	// if event is mouse enter
 	if (event === 'mouseenter') {
+		// burger menu item
 		if (element === 'burger') {
 			menuHighlight.textContent = 'The Green Giant';
 		}
+		// chicken menu item
 		if (element === 'pollo') {
 			menuHighlight.textContent = "Fried Chik'n and Fries";
 		}
+		// steak menu item
 		if (element === 'bistec') {
 			menuHighlight.textContent = 'Churrasco Verde';
 		}
-	} else if (event === 'mouseleave') {
+	}
+	// else if event is mouse leave
+	else if (event === 'mouseleave') {
+		// default text
 		menuHighlight.textContent = text;
 	}
-
-	console.log(event, text);
 };
+// add event listener on menu item elements
 bistec.onmouseenter = menuItemHover;
 bistec.onmouseleave = menuItemHover;
 pollo.onmouseenter = menuItemHover;
